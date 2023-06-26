d5849f2126fc67c6229859b168fe318f2d0997c7
Diana Kalstein <dianakalstein@gmail.com>
2023-06-26 14:24:11 +0200

    Split files

---
+++ src/UploadImage.jsx
@@ -0,0 +1,20 @@
+// import React, {useState} from "react";
+
+
+
+
+// function UploadImage() {
+// const [heading, setHeading] = useState("")
+// const [description, setDescription] = useState("")
+
+// return (
+//     <form>
+//         <label>
+//             <input type="text" name="h"/>
+//         </label>
+//     </form>
+
+// )
+// }
+
+// export default UploadImage;
